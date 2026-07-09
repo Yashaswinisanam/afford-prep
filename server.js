6b89eccb651231b1a06c4016e330ada19716d9a6
Yashaswinisanam <sanamyashaswini@gmail.com>
2026-07-09 22:37:30 +0530

    Add basic express server with about and health with process.uptime()

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const app=express();
 app.get('/',(req,res)=>{
     res.json({message:'server is listening '});
 });
+app.get("/about", (req, res) => {
+  res.json({
+    author: "Yashaswini",
+    role: "Full Stack Developer Candidate"
+  });
+});
+app.get("/health", (req, res) => {
+  res.json({
+    status: "OK",
+    uptime: process.uptime()
+  });
+});
 app.listen(3000,()=>{
     console.log('server is running on port 3000');
 });
